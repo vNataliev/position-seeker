8f22e41518be78c2d36df07e631984d33f9281ae
Your Name <you@example.com>
2022-10-02 16:06:54 +0200

    Adding description of position in the result at the end

--- src/App.js
+++ src/App.js
@@ -10,50 +10,54 @@ import QuestionsSoftware from "../src/questions/questions_software.json";
 import QuestionsTesting from "../src/questions/questions_testing.json";
 
 function App() {
+
   const [showResults, setShowResults] = useState(false);
+  const [result, setResult] = useState(0);
   const [showPreliminaryQuestions, setPreliminaryQuestions] = useState(true);
   const [currentPreliminaryQuestion, setCurrentPreliminaryQuestion] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [result, setResult] = useState(0);
-
+  const [description, setDescription] = useState(0);
 
   const clickAddingOption = () => {
     if (currentPreliminaryQuestion + 1 < QuestionsPreliminary.length) {
       setCurrentPreliminaryQuestion(currentPreliminaryQuestion + 1);
     } else {
       setPreliminaryQuestions(false);
-      setCurrentPreliminaryQuestion(0);
     }
   }
 
-  const clickTreeOption = (nextOption, resultOption) => {
+  const clickTreeOption = (nextOption, resultOption, resultDescription) => {
     if (nextOption != null) {
       setCurrentQuestion(nextOption-1);
     } else {
       setResult(resultOption);
+      setDescription(resultDescription);
       setShowResults(true);
     }
   }
 
   const restartGame = () => {
+    setShowResults(false);
     setCurrentQuestion(0);
     setCurrentPreliminaryQuestion(0);
-    setShowResults(false);
     setPreliminaryQuestions(true);
   };
 
   return (
     
-
     <div className="App">
 
       {showResults ? (
+
         <div className="final-results">
           <h1>Final Results</h1>
           <h3>{result}</h3>
+          <h3>{description}</h3>
           <button onClick={() => restartGame()}>Restart game</button>
         </div>
+
       ) : ( showPreliminaryQuestions ? (
+
         <><h1>Work position IT seeker</h1><h2>Question: {currentPreliminaryQuestion + 1} out of {QuestionsPreliminary.length}</h2><h3 className="question-text">{QuestionsPreliminary[currentPreliminaryQuestion].question}</h3><ul>
             {QuestionsPreliminary[currentPreliminaryQuestion].answers.map((option) => {
               return (
@@ -66,19 +70,22 @@ function App() {
               );
             })}
           </ul></>
+
       ) : ( 
+
         <><h1>Work position IT seeker</h1><h3 className="question-text">{QuestionsCybersecurity[currentQuestion].question}</h3><ul>
             {QuestionsCybersecurity[currentQuestion].answers.map((option) => {
               return (
                 <li
                   key={option.id}
-                  onClick={() => clickTreeOption(option.next, option.result)}
+                  onClick={() => clickTreeOption(option.next, option.result, option.description)}
                 >
                   {option.text}
                 </li>
               );
             })}
           </ul></>
+
       ))}
         
     </div>
