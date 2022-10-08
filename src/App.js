91f2a21693583190a8eab7a56108480a96db2eaf
Milena Milewczyk <milewczykmilena@gmail.com>
2022-10-08 22:34:12 +0200

    Added category_descriptions.json and preliminary_questions.json and also changes in App.js including decideOnCategory(),attributes and structure.

--- src/App.js
+++ src/App.js
@@ -8,106 +8,208 @@ import QuestionsInfrastructure from "../src/questions/questions_it_infrastructur
 import QuestionsManagement from "../src/questions/questions_management.json";
 import QuestionsSoftware from "../src/questions/questions_software.json";
 import QuestionsTesting from "../src/questions/questions_testing.json";
+import QuestionsCategory from "../src/questions/category_descriptions.json";
+
+let scoreSoftware = 0
+let scoreItInfra = 0
+let scoreDesign = 0
+let scoreData = 0
+let scoreCybersecurity = 0
+let scoreManagement = 0
+let scoreTesting = 0
+let scoreList = []
 
 function App() {
 
-  const [showStart, setShowStart] = useState(true);
+  const [currentPreliminaryQuestion, setCurrentPreliminaryQuestion] = useState(0);
+  const [showPreliminaryQuestions, setPreliminaryQuestions] = useState(true);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showResults, setShowResults] = useState(false);
   const [result, setResult] = useState(0);
-  const [showPreliminaryQuestions, setPreliminaryQuestions] = useState(true);
-  const [currentPreliminaryQuestion, setCurrentPreliminaryQuestion] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [description, setDescription] = useState(0);
+  const [category, setCategory] = useState('')
+  const [showCategories, setShowCategories] = useState(false)
 
-  const clickAddingOption = () => {
-    if (currentPreliminaryQuestion + 1 < QuestionsPreliminary.length) {
-      setCurrentPreliminaryQuestion(currentPreliminaryQuestion + 1);
-    } else {
-      setPreliminaryQuestions(false);
+  const lastPreliminaryQuestionConfig = () => {
+    setPreliminaryQuestions(false)
+    scoreList = {
+      "Software": scoreSoftware,
+      "IT Infrastructure": scoreItInfra,
+      "Design": scoreDesign,
+      "Data": scoreData,
+      "Cybersecurity": scoreCybersecurity,
+      "Managament": scoreManagement,
+      "Testing": scoreTesting
     }
+    decideOnGroup()
   }
 
-  const clickTreeOption = (nextOption, resultOption, resultDescription) => {
+  const clickAddingOption = (text) => {
+    if (currentPreliminaryQuestion < QuestionsPreliminary.length) {      
+      if (text === 'Yes') {
+
+        const path = QuestionsPreliminary[currentPreliminaryQuestion].answers[0]
+        
+        scoreSoftware += path.score_software
+        scoreItInfra += path.score_it_infra
+        scoreDesign += path.score_design
+        scoreData += path.score_data
+        scoreCybersecurity += path.score_cybersecurity
+        scoreManagement += path.score_management
+        scoreTesting += path.score_testing
+
+      }
+
+      // dodać punkty za "nie" ("No")
+
+      setCurrentPreliminaryQuestion(currentPreliminaryQuestion + 1)
+    } 
+  }
+
+  const decideOnGroup = () => {
+    console.log(scoreList)
+    let max = {"key": 0}
+
+    for(const [key, value] of Object.entries(scoreList)) {
+      if(value > Object.values(max)[0]) {
+        max = {[key]: value}
+      }
+      else if (value === Object.values(max)[0]) {
+        max[key] = value
+      }
+    }
+
+    if(Object.keys(max).length > 1) {
+      setShowCategories(true)
+
+      // TODO tutaj odpalić opcję wyświetlenia tego słownika z maxami jesli ich jest wiecej niz jedna kategoria i potem wynik przekazac do setCategory
+    //"Which category sounds the most interesting for you? (Wraz z opisami danej kategorii)
+    }
+    else {
+      console.log(Object.keys(max)[0])
+      switch(Object.keys(max)[0]){
+        case 'Software':
+          setCategory(QuestionsSoftware);
+          break;
+        case 'IT Infrastructure':
+          setCategory(QuestionsInfrastructure);
+          break;
+        case 'Design':
+          setCategory(QuestionsDesign);
+          break;
+        case 'Data':
+          setCategory(QuestionsData);
+          break;
+        case 'Cybersecurity':
+          setCategory(QuestionsCybersecurity);
+          break;
+        case 'Managament':
+          setCategory(QuestionsManagement);
+          break;
+        case 'Testing':
+          setCategory(QuestionsTesting);
+          break;
+      }
+    }
+    console.log(category)
+    
+  }
+
+   
+  const clickTreeOption = (nextOption, resultOption) => {
     if (nextOption != null) {
       setCurrentQuestion(nextOption-1);
     } else {
       setResult(resultOption);
-      setDescription(resultDescription);
       setShowResults(true);
     }
   }
 
-  const startQuiz = () => {
-    setShowStart(false);
-  }
-
-  const restartQuiz = () => {
-    setShowStart(true);
-    setShowResults(false);
-    setCurrentQuestion(0);
-    setCurrentPreliminaryQuestion(0);
-    setPreliminaryQuestions(true);
+  const restartGame = () => {
+    // setCurrentQuestion(0);
+    // setCurrentPreliminaryQuestion(0);
+    // setShowResults(false);
+    // setPreliminaryQuestions(true);
+    // setScoreSoftware(0);
+    // setScoreItInfra(0);
+    // setScoreDesign(0);
+    // setScoreData(0);
+    // setScoreCybersecurity(0);
+    // setScoreManagement(0);
+    // setScoreTesting(0);
+   // setScoreList(0);
   };
 
   return (
     
+
     <div className="App">
 
-      {showStart ? (
-
-        <div className="start-page">
-          <h1>Work position IT seeker</h1>
-          <p>aaa</p>
-          <button onClick={() => startQuiz()}>Start</button>
-        </div>
-
-      ) : ( showResults ? (
-
+      {
+        showResults ? (
           <div className="final-results">
             <h1>Final Results</h1>
             <h3>{result}</h3>
-            <p>{description}</p>
-            <button onClick={() => restartQuiz()}>Restart</button>
+            <button onClick={() => restartGame()}>Restart game</button>
           </div>
-
-        ) : ( showPreliminaryQuestions ? (
-
-          <div className="preliminary-questions">
-            <h1>Work position IT seeker</h1>
-            <h2>Question: {currentPreliminaryQuestion + 1} out of {QuestionsPreliminary.length}</h2><h3 className="question-text">{QuestionsPreliminary[currentPreliminaryQuestion].question}</h3><ul>
+        ) : ( 
+        showPreliminaryQuestions ? (
+          <><h1>Work position IT seeker</h1>
+          <h2>Question: {currentPreliminaryQuestion + 1} out of {QuestionsPreliminary.length}</h2>
+          <h3 className="question-text">{QuestionsPreliminary[currentPreliminaryQuestion].question}</h3>
+          <ul>
               {QuestionsPreliminary[currentPreliminaryQuestion].answers.map((option) => {
                 return (
                   <li
                     key={option.id}
-                    onClick={() => clickAddingOption()}
+                    onClick={() => {
+                      if (currentPreliminaryQuestion < QuestionsPreliminary.length - 1) {
+                        clickAddingOption(option.text)
+                      } else {
+                        clickAddingOption(option.text)
+                        lastPreliminaryQuestionConfig()
+                      }
+                    }
+                    }
                   >
                     {option.text}
                   </li>
                 );
               })}
-            </ul>
-          </div>
-
+            </ul></>
         ) : ( 
-
-          <div className="tree-questions">
-            <h1>Work position IT seeker</h1>
-            <h3 className="question-text">{QuestionsCybersecurity[currentQuestion].question}</h3><ul>
-              {QuestionsCybersecurity[currentQuestion].answers.map((option) => {
+        showCategories ? (
+            <>
+            <h1>Which category?</h1>
+            <ul>
+              {QuestionsCategory.map((option) => {
                 return (
                   <li
                     key={option.id}
-                    onClick={() => clickTreeOption(option.next, option.result, option.description)}
+                    onClick={() => {}}
+                  >
+                    {option.description}
+                  </li>
+                );
+              })}
+            </ul>
+            </>
+        ) : ( 
+          <><h1>Work position IT seeker</h1>
+          <h3 className="question-text">{category[currentQuestion].question}</h3>
+          <ul>
+              {category[currentQuestion].answers.map((option) => {
+                return (
+                  <li
+                    key={option.id}
+                    onClick={() => {clickTreeOption(option.next, option.result)}}
                   >
                     {option.text}
                   </li>
                 );
               })}
-            </ul>
-            </div>
-
-        ))
-      )}
+            </ul></>
+      )))
+      }
         
     </div>
   );
